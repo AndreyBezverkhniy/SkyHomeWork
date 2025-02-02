f42a95716db6e365da0c50b8fe3fc5e3ae02609c
banbezv <banbezv@mail.ru>
2025-02-02 18:34:58 +0300

    lA

---
+++ src/Lesson/Lesson.java
@@ -0,0 +1,14 @@
+package Lesson;
+
+public class Lesson {
+    public static void main(String[] args) {
+        qwe123
+        char ch='0'-1;
+        System.out.println(ch);
+        123qwe
+        byte a=1,b=2;
+        qwe
+        byte c = (byte) (a + b);
+        123
+    }
+}
