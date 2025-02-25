ca4fd855c273ef4e3886ef4010c3618e97c01d49
banbezv <banbezv@mail.ru>
2025-02-25 12:35:40 +0300

    HW-9

--- src/Lesson/Lesson.java
+++ src/Lesson/Lesson.java
@@ -1,20 +1,9 @@
 package Lesson;
 
-import java.util.Arrays;
-
-class C {
-    int x;
-    static char y;
-}
-
 class Lesson {
     public static void main(String[] args) {
-        int[] arr={1,2,3};
-        int sum=0;
-        for (int element : arr) {
-            sum += element;
-            sum=sum;
-            System.out.println(sum - sum);
-        }
+        String str="123";
+        String str2 = "123";
+        System.out.println(str == str2);
     }
 }
