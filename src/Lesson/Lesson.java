fa0bce2bc50b950fe2498bd0b4b231225274a01a
banbezv <banbezv@mail.ru>
2025-02-25 23:35:37 +0300

    HW-10 fixed

--- src/Lesson/Lesson.java
+++ src/Lesson/Lesson.java
@@ -1,9 +1,21 @@
 package Lesson;
 
 class Lesson {
+    public static void printSeparator() {
+        System.out.println("========");
+        System.out.println("--------");
+    }
     public static void main(String[] args) {
-        String str="123";
+        String str = "123";
         String str2 = "123";
         System.out.println(str == str2);
+        printSeparator();
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+            if ((i+1)%2==0) {
+                printSeparator();
+            }
+        }
+        printSeparator();
     }
 }
